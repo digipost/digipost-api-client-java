68b3de44f827f0ec744a47b31d0591fc7faec330
Frode Nerbråten <frode@nerbraten.no>
2012-11-28 13:01:41 +0100

    Added fields in NameAndAddress

--- src/main/java/no/digipost/api/client/representations/NameAndAddress.java
+++ src/main/java/no/digipost/api/client/representations/NameAndAddress.java
@@ -18,7 +18,12 @@ package no.digipost.api.client.representations;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import no.digipost.api.client.representations.xml.DateXmlAdapter;
+import org.joda.time.LocalDate;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "name-and-address", propOrder = {
@@ -26,10 +31,12 @@ import javax.xml.bind.annotation.XmlType;
     "addressline1",
     "addressline2",
     "postalcode",
-    "city"
+    "city",
+	"birthDate",
+	"phoneNumber",
+	"emailAddress"
 })
 public class NameAndAddress {
-
     @XmlElement(required = true)
     protected String fullname;
     @XmlElement(required = true)
@@ -40,5 +47,35 @@ public class NameAndAddress {
     protected String postalcode;
     @XmlElement(required = true)
     protected String city;
+	@XmlElement(name = "birth-date", type = String.class, nillable = false)
+	@XmlJavaTypeAdapter(DateXmlAdapter.class)
+	@XmlSchemaType(name = "date")
+	protected LocalDate birthDate;
+	@XmlElement(name = "phone-number", nillable = false)
+	protected String phoneNumber;
+	@XmlElement(name = "email-address", nillable = false)
+	protected String emailAddress;
 
+	NameAndAddress() {
+	}
+
+	public NameAndAddress(final String fullname, final String addressline1, final String addressline2, final String postalcode, final String city) {
+		this.fullname = fullname;
+		this.addressline1 = addressline1;
+		this.addressline2 = addressline2;
+		this.postalcode = postalcode;
+		this.city = city;
+	}
+
+	public NameAndAddress(final String fullname, final String addressline1, final String addressline2, final String postalcode, final String city,
+						  final LocalDate birthDate, final String phoneNumber, final String emailAddress) {
+		this.fullname = fullname;
+		this.addressline1 = addressline1;
+		this.addressline2 = addressline2;
+		this.postalcode = postalcode;
+		this.city = city;
+		this.birthDate = birthDate;
+		this.phoneNumber = phoneNumber;
+		this.emailAddress = emailAddress;
+	}
 }
