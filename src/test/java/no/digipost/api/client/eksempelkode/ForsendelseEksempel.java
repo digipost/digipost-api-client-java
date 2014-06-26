76ee3267190ff4a9f68967de93e6e7e85ba77b0e
Sindre Nordbø <sindre.nordbo@bekk.no>
2014-06-26 12:46:11 +0200

    Cleaned up some example code

--- src/test/java/no/digipost/api/client/eksempelkode/ForsendelseEksempel.java
+++ src/test/java/no/digipost/api/client/eksempelkode/ForsendelseEksempel.java
@@ -39,10 +39,10 @@ import static no.digipost.api.client.representations.SensitivityLevel.NORMAL;
 public class ForsendelseEksempel {
 
 	// Din virksomhets Digipost-kontoid
-	private static final long AVSENDERS_KONTOID = 1001;
+	private static final long AVSENDERS_KONTOID = 10987;
 
 	// Passordet sertifikatfilen er beskyttet med
-	private static final String SERTIFIKAT_PASSORD = "Qwer1234";
+	private static final String SERTIFIKAT_PASSORD = "SertifikatPassord123";
 
 	public static void main(final String[] args) {
 
@@ -51,10 +51,10 @@ public class ForsendelseEksempel {
 		InputStream sertifikatInputStream = lesInnSertifikat();
 
 		// 2. Vi oppretter en DigipostClient
-		DigipostClient client = new DigipostClient(DeliveryMethod.STEPWISE_REST, "http://localhost:8282", AVSENDERS_KONTOID, sertifikatInputStream, SERTIFIKAT_PASSORD);
+		DigipostClient client = new DigipostClient(DeliveryMethod.STEPWISE_REST, "https://api.digipost.no", AVSENDERS_KONTOID, sertifikatInputStream, SERTIFIKAT_PASSORD);
 
 		// 3. Vi oppretter et fødselsnummerobjekt
-		PersonalIdentificationNumber pin = new PersonalIdentificationNumber("01013300001");
+		PersonalIdentificationNumber pin = new PersonalIdentificationNumber("26079833787");
 
 		// 4. Vi oppretter hoveddokumentet
 		Document primaryDocument = new Document(UUID.randomUUID().toString(), "Dokumentets emne",
@@ -73,19 +73,14 @@ public class ForsendelseEksempel {
 	}
 
 	private static InputStream getPrimaryDocumentContent() {
-		try {
-			// Leser inn sertifikatet
-			return new FileInputStream(new File("/Users/sindrenordbo/_print.pdf"));
-		} catch (FileNotFoundException e) {
-			// Håndter at sertifikatet ikke kunne leses!
-			throw new RuntimeException("Kunne ikke lese sertifikatfil: " + e.getMessage(), e);
-		}
+		// Her må du returnere brevinnholdet du ønsker å sende istedenfor null
+		return null;
 	}
 
 	private static InputStream lesInnSertifikat() {
 		try {
 			// Leser inn sertifikatet
-			return new FileInputStream(new File("/Users/sindrenordbo/certificate-LOCAL-hafslund.p12"));
+			return new FileInputStream(new File("/path/til/sertifikatfil.p12"));
 		} catch (FileNotFoundException e) {
 			// Håndter at sertifikatet ikke kunne leses!
 			throw new RuntimeException("Kunne ikke lese sertifikatfil: " + e.getMessage(), e);
