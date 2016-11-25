01d606a45bac0cf6413bc6c311f207f68ceef7b3
Frode Nerbråten <frode.nerbraten@bekk.no>
2016-11-25 10:28:06 +0100

    Make loading of private key from p12 file more robust

---
+++ src/test/java/no/digipost/api/client/security/CryptoUtilTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package no.digipost.api.client.security;
+
+import org.junit.Test;
+
+import java.security.PrivateKey;
+
+import static org.junit.Assert.assertNotNull;
+
+public class CryptoUtilTest {
+
+	@Test
+	public void shouldLoadPrivateKeyFromPKCS12File() {
+		final PrivateKey privateKey = CryptoUtil.loadKeyFromP12(getClass().getResourceAsStream("certificate.p12"), "Qwer12345");
+		assertNotNull(privateKey);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void shouldThrowRuntimeExceptionWhenBadPassword() {
+		CryptoUtil.loadKeyFromP12(getClass().getResourceAsStream("certificate.p12"), "");
+	}
+}
