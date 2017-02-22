ec135c176e756f51900710b74bee8557481d5355
Frode Nerbråten <frode.nerbraten@bekk.no>
2017-02-22 15:18:22 +0100

    Do not validate content checksum when no content + cleanup error handling

--- src/main/java/no/digipost/api/client/filters/response/ResponseContentSHA256Interceptor.java
+++ src/main/java/no/digipost/api/client/filters/response/ResponseContentSHA256Interceptor.java
@@ -16,8 +16,9 @@
 package no.digipost.api.client.filters.response;
 
 import no.digipost.api.client.errorhandling.DigipostClientException;
-import no.digipost.api.client.util.LoggingUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpResponseInterceptor;
@@ -26,57 +27,28 @@ import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 import org.bouncycastle.crypto.digests.SHA256Digest;
 import org.bouncycastle.util.encoders.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Optional;
 
 import static no.digipost.api.client.Headers.X_Content_SHA256;
 import static no.digipost.api.client.errorhandling.ErrorCode.SERVER_SIGNATURE_ERROR;
-import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class ResponseContentSHA256Interceptor implements HttpResponseInterceptor {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ResponseContentSHA256Interceptor.class);
-
-    private boolean shouldThrow = true;
-
-    public void setThrowOnError(final boolean shouldThrow) {
-        this.shouldThrow = shouldThrow;
-    }
-
-
     @Override
     public void process(HttpResponse response, HttpContext context) throws HttpException, IOException {
-        if (response.getEntity() != null) {
-            try {
-                validerContentHash(response);
-            } catch (Exception e) {
-                LoggingUtil.logResponse(response);
-                logOrThrow("Det skjedde en feil under signatursjekk: " + e.getMessage(), e);
-            }
-        }
-    }
-
-    private void validerContentHash(final HttpResponse response) {
-        try {
-            String hashHeader = null;
-            Header firstHeader = response.getFirstHeader(X_Content_SHA256);
-            if(firstHeader != null){
-                hashHeader = firstHeader.getValue();
-            }
-
-            if (isBlank(hashHeader)) {
-                throw new DigipostClientException(SERVER_SIGNATURE_ERROR,
-                        "Mangler X-Content-SHA256-header - server-signatur kunne ikke valideres");
-            }
-            byte[] entityBytes = Optional.ofNullable(EntityUtils.toByteArray(response.getEntity())).orElseGet(() -> new byte[0]);
+        final HttpEntity entity = response.getEntity();
+        if (entity != null && entity.getContent() != null) {
+            String hashHeaderValue = Optional.ofNullable(response.getFirstHeader(X_Content_SHA256))
+                    .map(Header::getValue)
+                    .filter(StringUtils::isNoneBlank)
+                    .orElseThrow(() -> new DigipostClientException(SERVER_SIGNATURE_ERROR,
+                            String.format("Missing %s header in response. This header is expected when a response body is present. Http response was %s",
+                                    X_Content_SHA256, response.getStatusLine())));
+            byte[] entityBytes = EntityUtils.toByteArray(entity);
+            validerBytesMotHashHeader(hashHeaderValue, entityBytes);
             response.setEntity(new ByteArrayEntity(entityBytes));
-            validerBytesMotHashHeader(hashHeader, entityBytes);
-        } catch (IOException e) {
-            throw new DigipostClientException(SERVER_SIGNATURE_ERROR,
-                    "Det skjedde en feil under uthenting av innhold for validering av X-Content-SHA256-header - server-signatur kunne ikke valideres");
         }
     }
 
@@ -88,22 +60,7 @@ public class ResponseContentSHA256Interceptor implements HttpResponseInterceptor
         digest.doFinal(result, 0);
         String ourHash = new String(Base64.encode(result));
         if (!serverHash.equals(ourHash)) {
-            throw new DigipostClientException(SERVER_SIGNATURE_ERROR,
-                    "X-Content-SHA256-header matchet ikke innholdet - server-signatur er feil.");
-        }
-    }
-
-    private void logOrThrow(final String message, final Exception e) {
-        if (shouldThrow) {
-            if (e instanceof DigipostClientException) {
-                throw (DigipostClientException) e;
-            } else {
-                throw new DigipostClientException(SERVER_SIGNATURE_ERROR, message);
-            }
-        } else {
-            LOG.warn("Feil under validering av server signatur: '" + e.getMessage() + "'. " +
-                    (LOG.isDebugEnabled() ? "" : "Konfigurer debug-logging for " + LOG.getName() + " for å se full stacktrace."));
-            LOG.debug(e.getMessage(), e);
+            throw new DigipostClientException(SERVER_SIGNATURE_ERROR, X_Content_SHA256 + " header did not match actual response body");
         }
     }
 }
