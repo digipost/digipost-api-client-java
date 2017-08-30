adadc6ac3b6120096f6ff6ffc52d6cfc0e01f653
Frode Nerbråten <frode.nerbraten@bekk.no>
2017-08-30 12:56:16 +0200

    @XmlRootElement for inbox root element :D

--- src/main/java/no/digipost/api/client/representations/inbox/Inbox.java
+++ src/main/java/no/digipost/api/client/representations/inbox/Inbox.java
@@ -18,12 +18,14 @@ package no.digipost.api.client.representations.inbox;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 import java.util.ArrayList;
 import java.util.List;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "inbox")
+@XmlRootElement
 public class Inbox {
 
     @XmlElement(name = "document")
