6fc5bc3eaa43820a044c53013afacef57efe1839
Frode Nerbråten <frode@nerbraten.no>
2018-06-08 07:38:36 +0200

    Method ref to lambda because of compiler bug JDK-8175806

--- src/main/java/no/digipost/api/client/filters/response/ResponseContentSHA256Interceptor.java
+++ src/main/java/no/digipost/api/client/filters/response/ResponseContentSHA256Interceptor.java
@@ -41,7 +41,7 @@ public class ResponseContentSHA256Interceptor implements HttpResponseInterceptor
         final HttpEntity entity = response.getEntity();
         if (entity != null && entity.getContent() != null && entity.getContentLength() > 0) {
             String hashHeaderValue = Optional.ofNullable(response.getFirstHeader(X_Content_SHA256))
-                    .map(Header::getValue)
+                    .map(h -> h.getValue())
                     .filter(StringUtils::isNoneBlank)
                     .orElseThrow(() -> new DigipostClientException(SERVER_SIGNATURE_ERROR,
                             String.format("Missing %s header in response. This header is expected when a response body is present. Http response was %s",
