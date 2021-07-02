d7260f3573a22e88eac77968ff75ad81e3d1d626
Christian Strandenæs <christian.fredrik.strandenaes@posten.no>
2021-07-02 11:50:00 +0200

    add emaildetails in copyMessageWithOnlyDigipostDetails

--- src/main/java/no/digipost/api/client/representations/Message.java
+++ src/main/java/no/digipost/api/client/representations/Message.java
@@ -219,7 +219,8 @@ public class Message implements MayHaveSender {
 
         return new Message(messageToCopy.messageId, messageToCopy.senderId, messageToCopy.senderOrganization,
                 null, null, null, null, messageToCopy.deliveryTime, messageToCopy.invoiceReference,
-                messageToCopy.primaryDocument.copyDocumentAndSetDigipostFileTypeToPdf(), tmpAttachments, messageToCopy.recipient.getPrintDetails(), null, null);
+                messageToCopy.primaryDocument.copyDocumentAndSetDigipostFileTypeToPdf(), tmpAttachments,
+                messageToCopy.recipient.getPrintDetails(), null, null, null);
     }
 
     public static Message copyMessageWithOnlyDigipostDetails(Message messageToCopy){
@@ -227,18 +228,20 @@ public class Message implements MayHaveSender {
                 messageToCopy.recipient.nameAndAddress, messageToCopy.recipient.digipostAddress,
                 messageToCopy.recipient.personalIdentificationNumber, messageToCopy.recipient.organisationNumber,
                 messageToCopy.deliveryTime, messageToCopy.invoiceReference, messageToCopy.primaryDocument,
-                messageToCopy.attachments, null, messageToCopy.recipient.bankAccountNumber, messageToCopy.printIfUnread);
+                messageToCopy.attachments, null, messageToCopy.recipient.bankAccountNumber,
+                messageToCopy.printIfUnread, messageToCopy.recipient.emailDetails);
     }
 
-    private Message(final String messageId, final Long senderId, final SenderOrganization senderOrganization,
-                    final NameAndAddress nameAndAddress, final String digipostAddress, String personalIdentificationNumber,
-                    final String organisationNumber, final ZonedDateTime deliveryTime, final String invoiceReference,
-                    final Document primaryDocument, final List<Document> attachments, final PrintDetails printDetails, final String bankAccountNumber, PrintIfUnread printIfUnread){
+    private Message(String messageId, Long senderId, SenderOrganization senderOrganization,
+                    NameAndAddress nameAndAddress, String digipostAddress, String personalIdentificationNumber,
+                    String organisationNumber, ZonedDateTime deliveryTime, String invoiceReference,
+                    Document primaryDocument, List<Document> attachments, PrintDetails printDetails,
+                    String bankAccountNumber, PrintIfUnread printIfUnread, EmailDetails emailDetails){
         this.messageId = messageId;
         this.senderId = senderId;
         this.senderOrganization = senderOrganization;
         MessageRecipient recipient = new MessageRecipient(nameAndAddress, digipostAddress,
-                personalIdentificationNumber, organisationNumber, printDetails, bankAccountNumber);
+                personalIdentificationNumber, organisationNumber, printDetails, bankAccountNumber, emailDetails);
         this.recipient = recipient;
         this.deliveryTime = deliveryTime;
         this.invoiceReference = invoiceReference;
